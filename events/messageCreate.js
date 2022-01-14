8d96d2248a1744e003cfd5d54cfbef1b21e15ba2
OctoBanon <octobanonreserve@gmail.com>
2022-01-15 00:24:36 +0700

    Fixed code formating

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -4,15 +4,15 @@ const { prefix } = require("../config.json");
 const cooldowns = new Map();
 
 module.exports = async (client, message) => {
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).trim().split(/ +/);
     const commandName = args.shift().toLowerCase();
     const command = client.commands.get(commandName) ||
-    client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+        client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
-    if(!command) return;
+    if (!command) return;
 
-    if(!cooldowns.has(command.name)) {
+    if (!cooldowns.has(command.name)) {
         cooldowns.set(command.name, new Collection());
     };
 
@@ -20,10 +20,10 @@ module.exports = async (client, message) => {
     const timeStamps = cooldowns.get(command.name);
     const cooldownAmount = (command.cooldown) * 1000;
 
-    if(timeStamps.has(message.author.id)){
+    if (timeStamps.has(message.author.id)) {
         const expirationTime = timeStamps.get(message.author.id) + cooldownAmount;
 
-        if(currentTime < expirationTime){
+        if (currentTime < expirationTime) {
             const time_left = (expirationTime - currentTime) / 1000;
 
             return message.reply(`Please wait ${time_left.toFixed(1)} more seconds before using ${command.name}`);
@@ -33,9 +33,9 @@ module.exports = async (client, message) => {
     timeStamps.set(message.author.id, currentTime);
     setTimeout(() => timeStamps.delete(message.author.id), cooldownAmount);
 
-    if(command.permissions) {
+    if (command.permissions) {
         const cmdAuthorPerms = message.channel.permissionsFor(message.author);
-        if(!cmdAuthorPerms || !cmdAuthorPerms.has(command.permissions)) {
+        if (!cmdAuthorPerms || !cmdAuthorPerms.has(command.permissions)) {
             message.channel.send("You don't have permissions to use this command");
             return;
         };
@@ -43,7 +43,7 @@ module.exports = async (client, message) => {
 
     try {
         command.run(message, args, commandName, client);
-    } catch(error) {
+    } catch (error) {
         console.log(error);
     }
 }
