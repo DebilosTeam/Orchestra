8451b49241bad72c7f208daa6890ee8ec8ae751e
OctoBanon <octobanonreserve@gmail.com>
2022-01-15 00:05:06 +0700

    Cooldown testing feature

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,6 +1,8 @@
 const { MessageEmbed } = require("discord.js");
 const { prefix } = require("../config.json");
 
+const cooldowns = new Map();
+
 module.exports = async (client, message) => {
     if(!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).trim().split(/ +/);
@@ -9,6 +11,27 @@ module.exports = async (client, message) => {
     client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
     if(!command) return;
+
+    if(!cooldowns.has(command.name)) {
+        cooldowns.set(command.name, new Collection());
+    };
+
+    const currentTime = Date.now();
+    const timeStamps = cooldowns.get(command.name);
+    const cooldownAmount = (command.cooldown) * 1000;
+
+    if(timeStamps.has(message.author.id)){
+        const expirationTime = timeStamps.get(message.author.id) + cooldownAmount;
+
+        if(currentTime < expirationTime){
+            const time_left = (expirationTime - currentTime) / 1000;
+
+            return message.reply(`Please wait ${time_left.toFixed(1)} more seconds before using ${command.name}`);
+        }
+    }
+
+    timeStamps.set(message.author.id, currentTime);
+    setTimeout(() => timeStamps.delete(message.author.id), cooldownAmount);
 
     if(command.permissions) {
         const cmdAuthorPerms = message.channel.permissionsFor(message.author);
