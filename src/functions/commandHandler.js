5e491f2d19e93e7c52fcc1170b906cd64e8bd6ea
OctoBanon <octobanonreserve@gmail.com>
2022-01-17 14:03:48 +0700

    Rewrite command handler and changed function name in event handler

--- src/functions/commandHandler.js
+++ src/functions/commandHandler.js
@@ -1,16 +1,21 @@
-const fs = require("fs")
+const path = require("path");
 const colors = require('colors');
+const { readdir } = require("fs");
+
+/**
+ * @param {Client} client 
+ */
 
 module.exports = (client) => {
-    const commandFiles = fs.readdirSync("./src/commands/").filter(file => file.endsWith(".js"));
+    let commandDir = path.join(__dirname, "..", "commands");
+    readdir(commandDir, (err, files) => {
+        if (err) console.log(colors.red("[Error] ") + `${err}`);
 
-    for (const file of commandFiles) {
-        const command = require(`../commands/${file}`);
-        if (command.name) {
-            client.commands.set(command.name, command);
-            console.info(colors.green("[LOG] ") + `Command ${command.name} loaded`);
-        } else {
-            continue;
-        }
-    }
+        else
+            files.forEach((file) => {
+                const command = require(commandDir + "/" + file);
+                client.commands.set(command.name, command);
+                console.info(colors.green("[LOG] ") + `Command ${command.name} loaded`);
+            });
+    });
 }
--- src/functions/eventHandler.js
+++ src/functions/eventHandler.js
@@ -1,5 +1,5 @@
 const { Client } = require("discord.js");
-const fs = require("fs");
+const { readdir } = require("fs");
 const path = require("path");
 const colors = require('colors');
 
@@ -8,15 +8,15 @@ const colors = require('colors');
  */
 
 module.exports = async (client) => {
-    let EventsDir = path.join(__dirname, "..", "events");
-    fs.readdir(EventsDir, (err, files) => {
+    let eventsDir = path.join(__dirname, "..", "events");
+    readdir(eventsDir, (err, files) => {
         if (err) console.log(colors.red("[Error] ") + `${err}`);
 
         else
             files.forEach((file) => {
-                const event = require(EventsDir + "/" + file);
+                const event = require(eventsDir + "/" + file);
                 client.on(file.split(".")[0], event.bind(null, client));
-                console.log(colors.green("[LOG] ") + `Event ${file.split(".")[0]} loaded`);
+                console.info(colors.green("[LOG] ") + `Event ${file.split(".")[0]} loaded`);
             });
     });
 };
